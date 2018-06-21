03aecf16f3805b0da078a45e3b3679bf01d20be1
DmitryRusSPb <demontey@mail.ru>
2018-06-21 18:33:05 +0300

    Рабочий тестовый проект для записи и чтения данных с EEPROM по SPI интерфейсу

---
+++ Inc/defines.h
@@ -0,0 +1,19 @@
+/*
+ * defines.h
+ *
+ *  Created on: 21 июн. 2018 г.
+ *      Author: user
+ */
+
+#ifndef DEFINES_H_
+#define DEFINES_H_
+
+#define EEPROM_CS_Pin 			GPIO_PIN_6
+#define EEPROM_CS_GPIO_Port 	GPIOB
+//#define EEPROM_WP_Pin 			GPIO_PIN_1
+//#define EEPROM_WP_GPIO_Port 	GPIOB
+//#define EEPROM_HOLD_Pin 		GPIO_PIN_10
+//#define EEPROM_HOLD_GPIO_Port 	GPIOB
+
+
+#endif /* DEFINES_H_ */
